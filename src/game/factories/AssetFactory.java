a65ce3c72369721ea3023b9b6a3a490d6441adba
Jake Ferguson <jferguson15@my.wctc.edu>
2026-04-30 09:58:31 -0500

    build: Add Asset classes and AssetFactory

---
+++ src/game/factories/AssetFactory.java
@@ -0,0 +1,15 @@
+package game.factories;
+
+import game.assests.*;
+
+public class AssetFactory {
+
+    public Asset createAsset(String type) {
+        return switch (type.toLowerCase()){
+          case "key" -> new Key();
+          case "coin" -> new Coin();
+          case "lantern" -> new Lantern();
+            default -> throw new IllegalArgumentException("Unknown asset type: " + type);
+        };
+    }
+}
