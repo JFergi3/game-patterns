bb9c26e974e2ee3ff0a0bf700dd83395f1d7e010
Jake Ferguson <jferguson15@my.wctc.edu>
2026-05-02 13:05:46 -0500

    build: Search Strategy

--- src/game/strategies/SearchStrategy.java
+++ src/game/strategies/SearchStrategy.java
@@ -1,4 +1,23 @@
 package game.strategies;
 
-public class SearchStrategy {
+import game.Player;
+import game.Room;
+import game.assets.Asset;
+
+public class SearchStrategy implements ActionStrategy {
+
+    @Override
+    public void execute(Player player) {
+        Room room = player.getCurrentRoom();
+
+        System.out.println("You search the room...");
+
+        if (room.getAssets().isEmpty()) {
+            System.out.println("There is nothing useful here.");
+        } else {
+            for (Asset asset : room.getAssets()) {
+                System.out.println("You found: " + asset.getName());
+            }
+        }
+    }
 }
