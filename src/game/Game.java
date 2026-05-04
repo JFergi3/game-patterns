c9a6b4921535a1f2dbdeae53b0de355b123b24a1
Jake Ferguson <jferguson15@my.wctc.edu>
2026-05-04 09:13:06 -0500

    build: game loop/test refactor Main

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,4 +1,60 @@
 package game;
 
+import game.strategies.*;
+import java.util.Scanner;
+
 public class Game {
+
+    private GameManager gameManager;
+
+    public Game() {
+        gameManager = GameManager.getInstance();
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the game!");
+
+        while (gameManager.isGameRunning()) {
+            Player player = gameManager.getPlayer();
+
+            System.out.println("\nYou are in: " + player.getCurrentRoom().getName());
+            System.out.println(player.getCurrentRoom().getDescription());
+
+            System.out.print("Enter command (search, take, move, quit): ");
+            String input = scanner.nextLine().toLowerCase();
+
+            ActionStrategy action = null;
+
+            if (input.equals("search")) {
+                action = new SearchStrategy();
+            }
+            else if (input.startsWith("take")) {
+                String[] parts = input.split(" ");
+                if (parts.length > 1) {
+                    action = new TakeAssetStrategy(parts[1]);
+                }
+            }
+            else if (input.startsWith("move")) {
+                String[] parts = input.split(" ");
+                if (parts.length > 1) {
+                    action = new MoveStrategy(parts[1]);
+                }
+            }
+            else if (input.equals("quit")) {
+                gameManager.stopGame();
+                System.out.println("Game over.");
+                continue;
+            }
+
+            if (action != null) {
+                action.execute(player);
+            } else {
+                System.out.println("Invalid command.");
+            }
+        }
+
+        scanner.close();
+    }
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -2,10 +2,10 @@ package game;
 
 import game.assets.Asset;
 import game.factories.AssetFactory;
-import game.strategies.*;
 
 public class Main {
     public static void main(String[] args) {
+
         AssetFactory factory = new AssetFactory();
 
         Room entrance = new Room("Entrance", "You are standing at the entrance.");
@@ -20,30 +20,9 @@ public class Main {
         Player player = new Player("Player One");
         player.setCurrentRoom(entrance);
 
-        System.out.println(player.getName() + " is in " + player.getCurrentRoom().getName());
-        System.out.println(player.getCurrentRoom().getDescription());
-        System.out.println("Room has: " + key.getName());
-        System.out.println("North leads to: " + entrance.getExit("north").getName());
+        GameManager.getInstance().setPlayer(player);
 
-        //--Strategy Test ---
-
-        ActionStrategy search = new SearchStrategy();
-        search.execute(player);
-
-        ActionStrategy takeKey = new TakeAssetStrategy("key");
-        takeKey.execute(player);
-
-        System.out.println("Has key? " + player.hasAsset("key"));
-
-        ActionStrategy moveNorth = new MoveStrategy("north");
-        moveNorth.execute(player);
-
-        //--Singleton Test ---
-
-        GameManager gameManager = GameManager.getInstance();
-        gameManager.setPlayer(player);
-
-        System.out.println("Game running? " + gameManager.isGameRunning());
-        System.out.println("Current player: " + gameManager.getPlayer().getName());
+        Game game = new Game();
+        game.start();
     }
 }
