3c461a777f4172e0ec23c1b3875a369e0350d267
Jake Ferguson <jferguson15@my.wctc.edu>
2026-05-04 09:04:24 -0500

    build: Take Asset Strategy

--- src/game/strategies/TakeAssetStrategy.java
+++ src/game/strategies/TakeAssetStrategy.java
@@ -1,4 +1,29 @@
 package game.strategies;
 
-public class TakeAssetStrategy {
+import game.Player;
+import game.Room;
+import game.assets.Asset;
+
+public class TakeAssetStrategy implements ActionStrategy {
+    private String assetName;
+
+    public TakeAssetStrategy(String assetName) {
+        this.assetName = assetName;
+    }
+
+    @Override
+    public void execute(Player player) {
+        Room room = player.getCurrentRoom();
+
+        for (Asset asset : room.getAssets()) {
+            if (asset.getName().equalsIgnoreCase(assetName)) {
+                player.addAsset(asset);
+                room.removeAsset(asset);
+                System.out.println("You picked up: " + asset.getName());
+                return;
+            }
+        }
+
+        System.out.println("That item is not in this room.");
+    }
 }
