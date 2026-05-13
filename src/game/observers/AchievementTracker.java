2c1168061da196b65aa8a08b4988d3b95c246a0c
Jake Ferguson <jferguson15@my.wctc.edu>
2026-05-13 13:33:23 -0500

    Made UML easier to find/removed from package again

--- src/game/observers/AchievementTracker.java
+++ src/game/observers/AchievementTracker.java
@@ -4,7 +4,7 @@ public class AchievementTracker implements GameEventListener {
 
     @Override
     public void onEvent(String message) {
-        if (message.toLowerCase().contains("piacked up")){
+        if (message.toLowerCase().contains("picked up")){
             System.out.println("[Achievement] First item collected!");
         }
     }
